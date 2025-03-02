875c67b364652d7fe6cd6fb7e81504da50cb0e1e
Gabriel <gabyrivm06@gmail.com>
2025-03-01 21:50:22 -0400

    route connections frontend and backend

---
+++ backend/pages/api/routeConnections.js
@@ -0,0 +1,26 @@
+import Cors from 'cors';
+
+// Initialize the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
+  // Rest of the API logic
+  res.status(200).json({ message: 'DOHFOSDF:OKSDJFLSDKFJSLKDFJSLDKJFOEWHFLDKJS' });
+}
